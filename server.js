28a703e4d2a1ccb03eb4399dfea4224d50e1c14a
Yandy Viera <yvieram5@gmail.com>
2022-01-29 02:58:39 -0500

    Add port to env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const auth = require("json-server-auth");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-// const port = process.env.PORT || 3004;
+const port = process.env.PORT || 5000;
 
 // Bind the router db to the app
 app.db = router.db;
@@ -16,4 +16,4 @@ app.db = router.db;
 // app.use(rules);
 app.use(auth);
 app.use(router);
-app.listen(5000);
+app.listen(port);
