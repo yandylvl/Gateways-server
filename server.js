718a29a5949ff15f95d29fc65978bce6d44fe7ca
Yandy Viera <yvieram5@gmail.com>
2022-01-29 02:54:19 -0500

    Change default port to 5000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const auth = require("json-server-auth");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-const port = process.env.PORT || 3004;
+// const port = process.env.PORT || 3004;
 
 // Bind the router db to the app
 app.db = router.db;
@@ -16,4 +16,4 @@ app.db = router.db;
 // app.use(rules);
 app.use(auth);
 app.use(router);
-app.listen(3004);
+app.listen(5000);
