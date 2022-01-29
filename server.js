a3dc82979eeed68a7124df9b2016d0194e7eb988
Yandy Viera <yvieram5@gmail.com>
2022-01-29 03:33:10 -0500

    Add defaults middlewares

--- server.js
+++ server.js
@@ -3,19 +3,15 @@ const auth = require("json-server-auth");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults({ noCors: true });
+
 const port = process.env.PORT || 5000;
 
 // Bind the router db to the app
 app.db = router.db;
 
-// Permission rules
-// const rules = auth.rewriter({
-//   users: 600,
-// });
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-});
+// set default middlewares (logger, static, cors and no-cache)
+app.use(middlewares);
 app.use(auth);
 app.use(router);
 app.listen(port);
