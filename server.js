0bf4f8d672f8641c2f4414f16fbed89bcc32f779
Yandy Viera <yvieram5@gmail.com>
2022-01-29 03:56:12 -0500

    Add cors packages

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults({ noCors: true });
+// const middlewares = jsonServer.defaults({ noCors: true });
 
 const port = process.env.PORT || 5000;
 
@@ -11,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.db = router.db;
 
 // set default middlewares (logger, static, cors and no-cache)
-app.use(middlewares);
+app.use(cors());
 app.use(auth);
 app.use(router);
 app.listen(port);
