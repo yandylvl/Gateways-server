4bc617b7c7ecfc470a7dcdd445e7a6ec8c2c61c2
Yandy Viera <yvieram5@gmail.com>
2022-01-29 19:47:11 -0500

    Add msg when server is running

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const cors = require("cors");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults({ noCors: true });
 
 const port = process.env.PORT || 5000;
 
 // Bind the router db to the app
 app.db = router.db;
 
-// set default middlewares (logger, static, cors and no-cache)
 app.use(cors());
 app.use(auth);
 app.use(router);
-app.listen(port);
+app.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
